596394910fb3411c0d4d5041cf2bab24cf68e6a0
Murilo Sopi <dev.murilosopi@gmail.com>
2024-03-13 23:07:31 -0300

    adiciona propriedades de lista de participantes e interesses para Evento e Usuario

--- src/models/Evento.java
+++ src/models/Evento.java
@@ -1,12 +1,30 @@
 package models;
 
 import java.time.LocalDateTime;
-import models.CategoriaEvento;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Evento {
+	private int id;
 	private String nome;
 	private String endereco;
 	private CategoriaEvento categoria;
 	private LocalDateTime horario;
 	private String descricao;
+	private List<Usuario> participantes;
+	
+	public Evento(Evento e) {
+		this.id = e.id;
+		this.nome = e.nome;
+		this.endereco = e.endereco;
+		this.categoria = e.categoria;
+		this.horario = e.horario;
+		this.descricao = e.descricao;
+		
+		this.participantes = new ArrayList<Usuario>(e.participantes.size());
+	}
+
+	public int getId() {
+		return id;
+	}
 }
--- src/models/Usuario.java
+++ src/models/Usuario.java
@@ -1,8 +1,46 @@
 package models;
+
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class Usuario {
 	private int id;
 	private String nome;
 	private LocalDate dataNascimento;
+	private List<CategoriaEvento> interesses;
+	
+	public Usuario(Usuario u) {
+		this.id = u.id;
+		this.nome = u.nome;
+		this.dataNascimento = u.dataNascimento;
+		this.interesses = new ArrayList<>(u.interesses);
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dataNascimento, id, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(dataNascimento, other.dataNascimento) && id == other.id
+				&& Objects.equals(nome, other.nome);
+	}
+	
+	
+	
+		
 }
