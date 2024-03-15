6360dbc9f67f2eebf7981809f0f203c29cab5777
Murilo Sopi <dev.murilosopi@gmail.com>
2024-03-14 22:22:01 -0300

    add construtor de Evento

--- src/models/Evento.java
+++ src/models/Evento.java
@@ -9,16 +9,33 @@ public class Evento {
 	private String nome;
 	private String endereco;
 	private CategoriaEvento categoria;
-	private LocalDateTime horario;
+	private LocalDateTime dataHora;
 	private String descricao;
 	private List<Usuario> participantes;
+	
+	public Evento(String nome, String endereco, CategoriaEvento categoria, LocalDateTime dataHora, String descricao) {
+		/*
+		 * obter id do último e incrementar
+		 * */
+		
+		this.nome = nome;
+		this.endereco = endereco;
+		this.categoria = categoria;
+		this.dataHora = dataHora;
+		this.descricao = descricao;
+		
+		this.participantes = new ArrayList<Usuario>(participantes.size());
+		for(Usuario u : participantes) {
+			this.participantes.add(new Usuario(u));
+		}
+	}
 	
 	public Evento(Evento e) {
 		this.id = e.id;
 		this.nome = e.nome;
 		this.endereco = e.endereco;
 		this.categoria = e.categoria;
-		this.horario = e.horario;
+		this.dataHora = e.dataHora;
 		this.descricao = e.descricao;
 		
 		this.participantes = new ArrayList<Usuario>(e.participantes.size());
